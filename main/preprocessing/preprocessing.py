ff8a8e24655e2b3dbd0b712ad248d7a87a7704e9
Osama Ahmad <osama_ahmad@lums.net>
2024-09-27 19:19:48 +0500

    init

---
+++ main/preprocessing/preprocessing.py
@@ -0,0 +1,155 @@
+import os
+import argparse
+import numpy as np 
+import cv2
+import matplotlib.pyplot as plt
+import skimage as io
+from skimage.segmentation import slic, mark_boundaries
+from skimage.color import lab2rgb
+from skimage import graph
+from sklearn.model_selection import train_test_split
+import pickle
+import h5py
+import torch
+from torch_geometric.data import Data, Batch
+import torchvision.models as models
+from torch.nn import Sequential
+from torch_geometric.data import Data, Batch
+from torch_geometric.utils import to_dense_adj,to_dense_batch,dense_to_sparse
+from tqdm import tqdm
+
+class preprocessing:
+    def __init__(self,nodes=64,compactness=10,model_path=None):
+        self.nodes=int(nodes)
+        self.row=224
+        self.col=224
+        self.compactness=compactness
+        self.frame_len=3
+        self.model_path=model_path
+        vgg16=models.vgg16(weights='IMAGENET1K_V1')
+        self.new_model=Sequential(*list(vgg16.features.children())[:2])
+        self.new_model.eval()
+        
+    # Preprocess the dataset
+    def preprocess_image(self, image):
+        superpixels_mask = slic(image, n_segments=self.nodes, compactness=self.compactness)
+        # print(f"image shape: {image.shape}")  ## (224, 224, 3)
+        output_features = self.VGG16(torch.tensor(image, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0))
+        output_features = output_features.squeeze(0) ## C, H, W
+        node_features = self.compute_superpixel_features(image, superpixels_mask, output_features)
+        adjacency_matrix = self.compute_adjacency_matrix(image, superpixels_mask)
+        return torch.tensor(node_features, dtype=torch.float), torch.tensor(adjacency_matrix, dtype=torch.float)
+
+
+    def compute_superpixel_features(self,image, segments, output_features):
+        num_superpixels = int(np.max(segments))
+        ## featrues -> C, H, W      image -> H, W, C
+        superpixel_features = np.zeros((num_superpixels, 67))
+        output_features=output_features.permute(2,1,0).cpu().detach().numpy()
+        # _features=np.array(output_features.tolist())
+        # print(f"Output features shape: {output_features.shape}, Image shape: {image.shape}")
+        image=np.concatenate((image,output_features),axis=2)
+        for i in range(num_superpixels-1):
+            i+=1
+            mask = segments == i     
+            superpixel_pixels = image[mask.reshape(image.shape[:-1])]  # Reshape the mask to match image shape
+            avg_color = np.max(superpixel_pixels, axis=0)
+            superpixel_features[i] = avg_color
+        return superpixel_features
+
+    def compute_adjacency_matrix(self,image, superpixels):
+        # Create a region adjacency graph based on the mean color of each superpixel
+        rag = graph.rag_mean_color(image, superpixels)
+        # Convert RAG to adjacency matrix
+        num_superpixels = int(np.max(superpixels)) 
+        adjacency_matrix = np.zeros((num_superpixels, num_superpixels))
+        for edge in rag.edges():
+            i, j = edge
+            i-=1
+            j-=1
+            adjacency_matrix[i, j] = 1
+            adjacency_matrix[j, i] = 1
+        # edge_index = np.stack(np.where(adjacency_matrix == 1))
+        return adjacency_matrix
+
+    def VGG16(self,image):
+        output_features=self.new_model(image)
+        output_features=output_features.detach()
+        return output_features
+    
+    def split_data(self,data,labels): 
+        train_data, test_data, train_labels, test_labels=train_test_split(data,labels,test_size=0.15,random_state=42)
+        train_data, val_data, train_labels, val_labels=train_test_split(train_data,train_labels,test_size=0.15,random_state=42)
+        return train_data, train_labels, val_data, val_labels, test_data, test_labels
+    
+    def save_data(self, data_list, data_type):
+        # path = f"{self.model_path}/{data_type}_c2d2_BA.pkl"
+        path = f"{self.model_path}/{data_type}_10s.pkl"
+        print(f"\nSaving {data_type} data to {path}")
+        with open(path, "wb") as f:
+            pickle.dump(data_list, f)
+    def run(self):
+        file_path = os.path.join(self.model_path, "dataset-001.h5")
+        file = h5py.File(file_path)
+        labels = np.array(file['data_y'])
+        data = np.array(file['data_x'])
+        # select just 10 samples for testing
+        data = data[:10]
+        labels = labels[:10]
+        print("Data shape: ", data.shape, "Labels shape: ", labels.shape)
+        train_data, train_labels, val_data, val_labels, test_data, test_labels = self.split_data(data, labels)
+
+        datasets = {'train': (train_data, train_labels), 'val': (val_data, val_labels), 'test': (test_data, test_labels)}
+       
+        for index, (data_type, (_data, labels)) in tqdm(enumerate(datasets.items())):
+            print(f"\nProcessing {data_type} data")
+            data_list = []
+            for i in tqdm(range(len(_data))):
+                label = labels[i]
+                _images = _data[i] / 255.
+                adj={}
+                x={}
+                BA_size=self.nodes*self.frame_len
+                block_adj=torch.zeros((BA_size,BA_size))
+                prev_shape=0
+                for j in range(self.frame_len):
+                    image = cv2.resize(_images[j], (self.row, self.col))
+                    _x, adj[j] = self.preprocess_image(image)
+                    super_pixel_nodes=adj[j].shape[0]
+                    if  super_pixel_nodes <self.nodes:
+                        _adj=torch.cat((adj[j],torch.zeros((self.nodes-super_pixel_nodes,super_pixel_nodes))),axis=0)
+                        _adj=torch.cat((_adj,torch.zeros((self.nodes,self.nodes-super_pixel_nodes))),axis=1)
+                        x[j]=torch.cat((_x,torch.zeros((self.nodes-super_pixel_nodes,67))),axis=0)
+                    elif super_pixel_nodes>self.nodes:
+                        _adj=adj[j][:-1,:]
+                        _adj=_adj[:,:-1]
+                        x[j]=_x[:-1,:]
+                    else:
+                        x[j]=_x
+                        _adj=adj[j]
+                    new_shape=self.nodes+prev_shape
+                    block_adj[prev_shape:new_shape,prev_shape:new_shape]=_adj
+                    prev_shape=new_shape
+                    
+                _edge_attr=[adj[0][0].shape[0], adj[1][0].shape[0],adj[2][0].shape[0]] #edge attribute contains the information regarding the dynamic number of nodes 
+                _edge_index,_=dense_to_sparse(block_adj)
+                xnew = np.concatenate([x[0], x[1], x[2]], axis=0)
+                xnew = torch.tensor(xnew, dtype=torch.float)  # Convert numpy array to tensor
+
+                # xnew=torch.cat([x[0], x[1], x[2]],dim=0)
+                data = Data(x=xnew,edge_index=_edge_index,edge_attr=_edge_attr,y=torch.tensor([label], dtype=torch.float).squeeze(1))
+                data_list.append([data])
+        
+            self.save_data(data_list, data_type)
+            
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process some parameters.")
+    parser.add_argument("--nodes", type=int, default=64, help="Number of nodes for superpixels")
+    parser.add_argument("--compactness", type=float, default=10.0, help="Compactness for superpixel segmentation")
+    parser.add_argument("--path", type=str, default='data/', help="Path to the dataset directory")
+    
+    args = parser.parse_args()
+
+    processor = preprocessing(nodes=args.nodes, compactness=args.compactness, model_path=args.path)
+    processor.run()
+    
---
+++ main/src/utils/base.py
@@ -0,0 +1,116 @@
+import torch
+from tqdm import tqdm
+from torch_geometric.utils import to_dense_adj, to_dense_batch
+from sklearn.metrics import confusion_matrix, accuracy_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+import logging
+import numpy as np
+import os
+
+def plot_confusion_matrix(confusion_matrix):
+    plt.figure(figsize=(8, 8))
+    sns.heatmap(confusion_matrix, annot=True, fmt='d', cmap='Blues')
+    plt.xlabel('Predicted Class')
+    plt.ylabel('True Class')
+    plt.title('Confusion Matrix')
+    plt.show()
+
+class BaseEngine():
+    def __init__(self, model, optimizer, loss_fn, penalty, device, train_loader, val_loader, test_loader, num_epochs, show_cm=False):
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.penalty = penalty
+        self.device = device
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.test_loader = test_loader
+        self.num_epochs = num_epochs
+        self.show_cm = show_cm
+        
+        experiment_name = "new"
+        os.makedirs(f"experiments/{experiment_name}", exist_ok=True)
+        self.best_model_path = f"experiments/{experiment_name}/best_model.pt"
+        
+        # Set up logging
+        log_file_path = f"experiments/{experiment_name}/log.txt"
+        logging.basicConfig(level=logging.INFO, 
+                            format='%(asctime)s - %(levelname)s - %(message)s',
+                            handlers=[
+                                logging.FileHandler(log_file_path),
+                                logging.StreamHandler()
+                            ])
+        self.logger = logging.getLogger()
+
+        self.logger.info("Initialized BaseEngine")
+
+    def train_fn(self, loader):
+        self.model.train()
+        total_loss = 0
+        bar = tqdm(loader, bar_format='{l_bar}{bar:20}{r_bar}{bar:-20b}', ncols=100)
+
+        for i, data in enumerate(bar):
+            data = data[0]
+            target = data.y.long().to(self.device)
+            x, edge_index, batch = data.x, data.edge_index, data.batch
+            x, _ = to_dense_batch(x, batch)
+            x = x.to(self.device)
+            adj = to_dense_adj(edge_index, batch).to(self.device)
+            self.optimizer.zero_grad()
+            out, _, adja = self.model(x, adj, indices=None)
+            loss = self.loss_fn(out.float(), target) 
+            if adja is not None:
+                loss += self.penalty * torch.norm(adja, p=1)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
+            self.optimizer.step()
+            total_loss += loss.item()
+
+        avg_loss = total_loss / len(loader.dataset)
+        self.logger.info(f'Training loss: {avg_loss:.6f}')
+        return avg_loss
+
+    def evaluate(self, loader):
+        self.model.eval()
+        all_preds = []
+        all_labels = []
+        bar = tqdm(loader)
+        with torch.no_grad():
+            for i, data in enumerate(bar):
+                data = data[0]
+                label = data.y
+                x, edge_index, batch = data.x, data.edge_index, data.batch
+                x, _ = to_dense_batch(x, batch)
+                x = x.to(self.device)
+                adj = to_dense_adj(edge_index, batch).to(self.device)
+                logits, _, adja = self.model(x, adj, indices=None)
+                pred = torch.max(logits, 1)[1]
+                all_preds.extend(pred.tolist())
+                all_labels.extend(label.tolist())
+        accuracy = accuracy_score(all_labels, all_preds)
+        cm = confusion_matrix(all_labels, all_preds)
+        return accuracy, cm
+    
+    def train(self):
+        train_loss = []
+        best_val_accuracy = -np.inf
+        for epoch in range(self.num_epochs):
+            self.logger.info(f'Starting epoch {epoch + 1}/{self.num_epochs}')
+            loss = self.train_fn(self.train_loader)
+            val_accuracy, cm = self.evaluate(self.val_loader)
+            train_accuracy, cm = self.evaluate(self.train_loader)
+            if val_accuracy > best_val_accuracy:
+                best_val_accuracy = val_accuracy
+                torch.save(self.model.state_dict(), self.best_model_path)
+                self.logger.info(f"Best model saved at {self.best_model_path}")
+            self.logger.info(f'Epoch: {epoch + 1}, Loss: {loss:.16f}, Train Accuracy: {train_accuracy*100:.6f}%, Val Accuracy: {val_accuracy*100:.6f}%')
+            train_loss.append(loss)
+        self.evaluateTest(self.best_model_path)
+
+    def evaluateTest(self, model_path):
+        self.model.load_state_dict(torch.load(model_path, map_location=self.device, weights_only=True)) 
+        test_accuracy, cm = self.evaluate(self.test_loader)
+        self.logger.info(f'Test Accuracy: {test_accuracy*100:.2f}%')
+        if self.show_cm:
+            plot_confusion_matrix(cm)
